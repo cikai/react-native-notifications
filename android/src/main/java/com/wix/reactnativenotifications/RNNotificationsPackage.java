10c90517a304ce9f475c80d7f47555191d811332
Ci Kai <cikai0923@gmail.com>
2018-01-26 11:11:55 +0800

    add createJSModules( ) for React Native v0.44 compatibility.

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
@@ -26,6 +27,11 @@ public class RNNotificationsPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+    
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 }
